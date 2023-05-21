ddbe54a7b072167d14acec397972cc351107151d
ma4cek <ma4cek@posteo.de>
2023-05-21 20:14:30 +0200

    Commits analysis

--- src/action.ts
+++ src/action.ts
@@ -3,9 +3,9 @@ import {inc, parse, valid} from 'semver'
 import {createTag, getBranchFromRef, getLatestTag, getCommits} from './github'
 
 export default async function main(): Promise<void> {
-  const mainBranches: string[] = ['main', 'master']
+  const commitAnalyzer = require('@semantic-release/commit-analyzer')
 
-  const releaseNotesGenerator = require('@semantic-release/release-notes-generator')
+  const mainBranches: string[] = ['main', 'master']
 
   const tagPrefix = core.getInput('tag_prefix')
   const shouldFetchAllTags = core.getInput('fetch_all_tags')
@@ -58,6 +58,11 @@ export default async function main(): Promise<void> {
   core.setOutput('previous_version', previousVersion.version)
   core.setOutput('previous_tag', latestTag.name)
 
+  let bump = await commitAnalyzer.analyzeCommits(
+    {preset: 'angular'},
+    {commits: commits}
+  )
+
   const incrementedVersion = inc(previousVersion, 'patch')
 
   if (!incrementedVersion) {
@@ -76,38 +81,6 @@ export default async function main(): Promise<void> {
   const newTag = `${tagPrefix}${incrementedVersion}`
   core.info(`New tag after applying prefix is ${newTag}.`)
   core.setOutput('new_tag', newTag)
-
-  /**
-   * Generate the changelog for all the commits in `options.commits`.
-   *
-   * @param {Object} pluginConfig The plugin configuration.
-   * @param {String} pluginConfig.preset conventional-changelog preset ('angular', 'atom', 'codemirror', 'ember', 'eslint', 'express', 'jquery', 'jscs', 'jshint').
-   * @param {String} pluginConfig.config Requireable npm package with a custom conventional-changelog preset
-   * @param {Object} pluginConfig.parserOpts Additional `conventional-changelog-parser` options that will overwrite ones loaded by `preset` or `config`.
-   * @param {Object} pluginConfig.writerOpts Additional `conventional-changelog-writer` options that will overwrite ones loaded by `preset` or `config`.
-   * @param {Object} context The semantic-release context.
-   * @param {Array<Object>} context.commits The commits to analyze.
-   * @param {Object} context.lastRelease The last release with `gitHead` corresponding to the commit hash used to make the last release and `gitTag` corresponding to the git tag associated with `gitHead`.
-   * @param {Object} context.nextRelease The next release with `gitHead` corresponding to the commit hash used to make the  release, the release `version` and `gitTag` corresponding to the git tag associated with `gitHead`.
-   * @param {Object} context.options.repositoryUrl The git repository URL.
-   */
-
-  const changelog = await releaseNotesGenerator.generateNotes(
-    {
-      preset: 'conventionalcommits'
-    },
-    {
-      commits,
-      options: {
-        //repositoryUrl: `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}`
-        repositoryUrl: 'https://github.com/ma4cek/test.git'
-      },
-      lastRelease: {gitTag: latestTag.name},
-      nextRelease: {gitTag: newTag, version: incrementedVersion}
-    }
-  )
-  core.info(`Changelog is ${changelog}.`)
-  core.setOutput('changelog', changelog)
 
   await createTag(newTag, commitRef)
 }
